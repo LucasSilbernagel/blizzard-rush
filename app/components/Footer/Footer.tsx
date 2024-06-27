6d1743bb1e61a4126402416b3a212317c22b1e0e
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-06-26 22:18:15 -0400

    added shipping banner

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,8 +1,28 @@
+import { Link } from '@remix-run/react'
+import { RxCountdownTimer } from 'react-icons/rx'
+import { LiaShippingFastSolid } from 'react-icons/lia'
+import { FaCanadianMapleLeaf } from 'react-icons/fa'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="Footer">
+      <ul className="Footer__Shipping-banner">
+        <li>
+          <RxCountdownTimer />{' '}
+          <Link to="/easy-returns">30 Days Easy Returns</Link>
+        </li>
+        <li>
+          <LiaShippingFastSolid />{' '}
+          <Link to="/free-shipping">
+            Free Shipping on orders of $98+ (Regular Price Items)
+          </Link>
+        </li>
+        <li>
+          <FaCanadianMapleLeaf />{' '}
+          <Link to="/shipping-policy">We Ship from Canada</Link>
+        </li>
+      </ul>
       <p className="mb-2">
         Built by{' '}
         <a
