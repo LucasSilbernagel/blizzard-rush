fe822f14d769ad6ddcac861013618f44735d0d83
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-07-08 22:37:51 -0400

    testing another og image update

--- app/root.tsx
+++ app/root.tsx
@@ -50,6 +50,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="image"
+          content="https://blizzard-rush.vercel.app/homepage.png"
+        />
         <Meta />
         <Links />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
