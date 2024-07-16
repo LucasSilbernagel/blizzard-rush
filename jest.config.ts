a0461117331c6fac5fcb894ce208f56b32df794c
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-07-15 22:42:39 -0400

    added test for 404 page

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,8 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '^shadcn/(.*)$': '<rootDir>/shadcn/$1',
     '~/(.*)': '<rootDir>/app/$1',
+    '\\.(jpg|jpeg|png|gif|webp|svg|mp4|webm|ogg|mp3|wav|mpe?g|aac|woff2?|eot|ttf|otf|ico|pdf|zip)$':
+      '<rootDir>/app/mocks/fileMock.ts',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.tsx'],
   transformIgnorePatterns: [
