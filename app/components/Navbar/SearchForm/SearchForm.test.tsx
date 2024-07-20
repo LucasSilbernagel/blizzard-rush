d74c7e1cb69e86c443c9aba0ea5b5c14c67c9556
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-07-20 17:20:28 -0400

    added SearchForm test

---
+++ app/components/Navbar/SearchForm/SearchForm.test.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-extra-semi */
+import { render, screen } from '@testing-library/react'
+import SearchForm from './SearchForm'
+import { userEvent } from '@testing-library/user-event'
+
+test('renders correctly when the cart is empty', async () => {
+  render(<SearchForm />)
+  expect(screen.getByRole('searchbox')).toBeVisible()
+  expect(screen.getByLabelText('Submit product search')).toBeVisible()
+  expect(screen.getByText('Product search')).toBeInTheDocument()
+  expect(
+    screen.getByText('Search for Blizzard Rush products.')
+  ).toBeInTheDocument()
+  expect(
+    screen.queryByText('Search query must be at least 3 characters.')
+  ).not.toBeInTheDocument()
+  await userEvent.click(screen.getByLabelText('Submit product search'))
+  expect(
+    screen.getByText('Search query must be at least 3 characters.')
+  ).toBeVisible()
+  await userEvent.type(screen.getByRole('searchbox'), 'Snowboard')
+  expect(screen.getByRole('searchbox')).toHaveValue('Snowboard')
+  await userEvent.click(screen.getByLabelText('Submit product search'))
+  expect(
+    screen.queryByText('Search query must be at least 3 characters.')
+  ).not.toBeInTheDocument()
+})
