a3c37e177fe31bfcf41a39a41847ce59db23e0aa
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-07-25 22:54:51 -0400

    added e2e test delay

--- e2e/wishlist.spec.ts
+++ e2e/wishlist.spec.ts
@@ -15,6 +15,7 @@ test('adds a product to the wishlist on desktop', async ({ page }) => {
   await page.click('text=The 3p Fulfilled Snowboard')
   await expect(page).toHaveTitle(/Blizzard Rush | The 3p Fulfilled Snowboard/)
   await page.click('text=Add to wishlist')
+  await page.waitForTimeout(1000)
   await expect(
     page.locator('text=Added The 3p Fulfilled Snowboard to wishlist')
   ).toBeVisible()
@@ -48,6 +49,7 @@ test('adds a product to the wishlist on mobile', async ({ page }) => {
   await page.click('text=The 3p Fulfilled Snowboard')
   await expect(page).toHaveTitle(/Blizzard Rush | The 3p Fulfilled Snowboard/)
   await page.click('text=Add to wishlist')
+  await page.waitForTimeout(1000)
   await expect(
     page.locator('text=Added The 3p Fulfilled Snowboard to wishlist')
   ).toBeVisible()
